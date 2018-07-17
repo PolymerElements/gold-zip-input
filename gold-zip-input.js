c299add120720c93084792a47b982b351d81f9bf
Elliott Marquez <emarquez@google.com>
2018-07-16 17:25:24 -0700

    fix elements

--- gold-zip-input.js
+++ gold-zip-input.js
@@ -1,12 +1,23 @@
 /**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
+http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
+found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
+part of the polymer project is also subject to an additional IP rights grant
+found at http://polymer.github.io/PATENTS.txt
 */
+import '@polymer/polymer/polymer-legacy.js';
+import '@polymer/paper-input/paper-input-container.js';
+import '@polymer/paper-input/paper-input-error.js';
+import '@polymer/iron-input/iron-input.js';
+import './zip-validator.js';
+
+import {IronFormElementBehavior} from '@polymer/iron-form-element-behavior/iron-form-element-behavior.js';
+import {PaperInputBehavior} from '@polymer/paper-input/paper-input-behavior.js';
+import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import {html} from '@polymer/polymer/lib/utils/html-tag.js';
 /**
 `gold-zip-input` is a single-line text field with Material Design styling
 for entering a US zip code.
@@ -35,31 +46,11 @@ See `Polymer.PaperInputContainer` for a list of custom properties used to
 style this element.
 
 @group Gold Elements
-@hero hero.svg
 @demo demo/index.html
 @class gold-zip-input
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-import '@polymer/polymer/polymer-legacy.js';
-
-import { PaperInputBehavior } from '@polymer/paper-input/paper-input-behavior.js';
-import '@polymer/paper-input/paper-input-container.js';
-import '@polymer/paper-input/paper-input-error.js';
-import '@polymer/iron-input/iron-input.js';
-import { IronFormElementBehavior } from '@polymer/iron-form-element-behavior/iron-form-element-behavior.js';
-import './zip-validator.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-import { DomModule } from '@polymer/polymer/lib/elements/dom-module.js';
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-const $_documentContainer = document.createElement('template');
-$_documentContainer.setAttribute('style', 'display: none;');
-
-$_documentContainer.innerHTML = `<dom-module id="gold-zip-input">
-  <template>
+Polymer({
+  _template: html`
     <style>
       :host {
         display: block;
@@ -97,13 +88,42 @@ $_documentContainer.innerHTML = `<dom-module id="gold-zip-input">
       }
     </style>
 
-    <paper-input-container id="container" auto-validate="[[autoValidate]]" attr-for-value="bind-value" disabled\$="[[disabled]]" no-label-float="[[noLabelFloat]]" always-float-label="[[_computeAlwaysFloatLabel(alwaysFloatLabel,placeholder)]]" invalid="[[invalid]]">
-
-      <label slot="label" hidden\$="[[!label]]">[[label]]</label>
+    <paper-input-container
+        id="container"
+        auto-validate="[[autoValidate]]"
+        attr-for-value="bind-value"
+        disabled$="[[disabled]]"
+        no-label-float="[[noLabelFloat]]"
+        always-float-label="[[_computeAlwaysFloatLabel(alwaysFloatLabel,placeholder)]]"
+        invalid="[[invalid]]">
+      <label slot="label" hidden$="[[!label]]">[[label]]</label>
 
       <zip-validator></zip-validator>
 
-      <span id="template-placeholder"></span>
+      <iron-input
+          id="input"
+          slot="input"
+          allowed-pattern="[0-9\\-]"
+          bind-value="{{value}}"
+          invalid="{{invalid}}"
+          validator="zip-validator">
+        <input
+            id="nativeInput"
+            aria-labelledby$="[[_ariaLabelledBy]]"
+            aria-describedby$="[[_ariaDescribedBy]]"
+            required$="[[required]]"
+            type="tel"
+            maxlength="10"
+            autocomplete="postal-code"
+            name$="[[name]]"
+            disabled$="[[disabled]]"
+            autofocus$="[[autofocus]]"
+            inputmode$="[[inputmode]]"
+            placeholder$="[[placeholder]]"
+            readonly$="[[readonly]]"
+            size$="[[size]]"
+            on-change="_onChange">
+      </iron-input>
 
       <template is="dom-if" if="[[errorMessage]]">
         <paper-input-error slot="add-on" id="error">
@@ -112,65 +132,35 @@ $_documentContainer.innerHTML = `<dom-module id="gold-zip-input">
       </template>
 
     </paper-input-container>
-  </template>
-
-  <template id="v0">
-    <input is="iron-input" id="input" slot="input" aria-labelledby\$="[[_ariaLabelledBy]]" aria-describedby\$="[[_ariaDescribedBy]]" required\$="[[required]]" validator="zip-validator" type="tel" allowed-pattern="[0-9\\-]" prevent-invalid-input="" maxlength="10" bind-value="{{value}}" autocomplete="postal-code" name\$="[[name]]" disabled\$="[[disabled]]" invalid="{{invalid}}" autofocus\$="[[autofocus]]" inputmode\$="[[inputmode]]" placeholder\$="[[placeholder]]" readonly\$="[[readonly]]" size\$="[[size]]" on-change="_onChange">
-  </template>
-
-  <template id="v1">
-    <iron-input id="input" slot="input" allowed-pattern="[0-9\\-]" bind-value="{{value}}" invalid="{{invalid}}" validator="zip-validator">
-      <input id="nativeInput" aria-labelledby\$="[[_ariaLabelledBy]]" aria-describedby\$="[[_ariaDescribedBy]]" required\$="[[required]]" type="tel" maxlength="10" autocomplete="postal-code" name\$="[[name]]" disabled\$="[[disabled]]" autofocus\$="[[autofocus]]" inputmode\$="[[inputmode]]" placeholder\$="[[placeholder]]" readonly\$="[[readonly]]" size\$="[[size]]" on-change="_onChange">
-    </iron-input>
-  </template>
-
-  
-
-</dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
-Polymer({
-
+  `,
   is: 'gold-zip-input',
 
   behaviors: [PaperInputBehavior, IronFormElementBehavior],
 
   properties: {
-
     /**
      * The label for this input.
      */
-    label: {type: String, value: 'Zip Code'},
+    label: {
+      type: String,
+      value: 'Zip Code',
+    },
 
     value: {
       // Required for the correct TypeScript type-generation
-      type: String
+      type: String,
     }
 
   },
 
   observers: ['_computeValue(value)'],
 
-  beforeRegister: function() {
-    var template = DomModule.import('gold-zip-input', 'template');
-    var version = PolymerElement ? 'v1' : 'v0';
-    var inputTemplate =
-        DomModule.import('gold-zip-input', 'template#' + version);
-    var inputPlaceholder =
-        template.content.querySelector('#template-placeholder');
-    if (inputPlaceholder) {
-      inputPlaceholder.parentNode.replaceChild(
-          inputTemplate.content, inputPlaceholder);
-    }
-  },
-
   /**
    * Returns a reference to the focusable element. Overridden from
    * PaperInputBehavior to correctly focus the native input.
    */
   get _focusableElement() {
-    return PolymerElement ? this.inputElement._inputElement :
-                             this.inputElement;
+    return this.inputElement._inputElement;
   },
 
   // Note: This event is only available in the 2.0 version of this element.
